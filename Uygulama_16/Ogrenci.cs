5d58a44f2678fc00dcb07fb7af0948d498c8996a
amged <amged@DESKTOP-9MT6K87>
2023-08-20 13:14:49 +0300

    Add project files.

---
+++ Uygulama_16/Ogrenci.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama_16
+{
+    internal class Ogrenci
+    {
+        public string Ad;
+        public string Soyad;
+        public int Vize1;
+        public int Vize2;
+        public int Final;
+
+        public Ogrenci(string ad, string soyad)
+        {
+            Ad = ad;
+            Soyad = soyad;
+        }
+
+        public double OrtalamaHesapla()
+        {
+            return (Vize1 * 0.3) + (Vize2 * 0.3) + (Final * 0.4);
+        }
+
+        public void BilgileriYazdir()
+        {
+            Console.WriteLine("Öğrenci Bilgileri:");
+            Console.WriteLine("Ad: " + Ad);
+            Console.WriteLine("Soyad: " + Soyad);
+            Console.WriteLine("Vize 1: " + Vize1);
+            Console.WriteLine("Vize 2: " + Vize2);
+            Console.WriteLine("Final: " + Final);
+            Console.WriteLine("Ortalama: " + OrtalamaHesapla());
+
+            double ortalama = OrtalamaHesapla();
+            if (ortalama >= 50)
+            {
+                Console.WriteLine("***********************");
+                Console.WriteLine("Durum: Geçti");
+                Console.WriteLine("***********************");
+
+            }
+            else
+            {
+                Console.WriteLine("***********************");
+                Console.WriteLine("Durum: Kaldı");
+                Console.WriteLine("***********************");
+            }
+
+        }
+    }
+}
---
+++ Uygulama_16/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Üniversite Sınav Hesaplama Sistemi");
+
+            Console.Write("Öğrencinin Adını Giriniz: ");
+            string ad = Console.ReadLine();
+
+            Console.Write("Öğrencinin Soyadını Giriniz: ");
+            string soyad = Console.ReadLine();
+
+            Ogrenci ogrenci = new Ogrenci(ad, soyad);
+
+            Console.Write("Vize 1 Notunu Giriniz: ");
+            ogrenci.Vize1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Vize 2 Notunu Giriniz: ");
+            ogrenci.Vize2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Final Notunu Giriniz: ");
+            ogrenci.Final = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            ogrenci.BilgileriYazdir();
+
+
+            Console.ReadKey();
+        }
+
+        
+    }
+
+}
